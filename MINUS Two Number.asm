8844fa6b629028df3fd4591ca82c63f915df2e68
Sadi-Rahman <sadiakash1309@gmail.com>
2022-06-20 22:02:39 +0600

    Subtraction

---
+++ MINUS Two Number.asm
@@ -0,0 +1,25 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+MAIN PROC
+    
+        MOV AH,1
+        INT 21H
+        MOV BL,AL
+        
+        INT 21H
+        MOV BH,AL
+        
+        SUB BL,BH 
+        ADD BL,30H
+        
+        MOV AH,2
+        MOV DL,BL
+        INT 21H
+        
+        MOV AH,4CH
+        INT 21H
+      
+    MAIN ENDP
+END MAIN
